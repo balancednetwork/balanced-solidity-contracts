7dfb293e9d8ee67fa5b46881c2d03fa1b4234bc9
gcranju <qa5505815@gmail.com>
2024-06-13 16:52:37 +0545

    fix: balance issue

--- src/asset-manager/AssetManager.sol
+++ src/asset-manager/AssetManager.sol
@@ -139,6 +139,7 @@ contract AssetManager is
         uint amount,
         string memory to
     ) external payable {
+        console.log("to: ", to);
         _deposit(token, amount, to, "");
     }
 
@@ -174,7 +175,12 @@ contract AssetManager is
         bytes memory data
     ) internal {
         require(amount > 0, "Amount less than minimum amount");
+        uint256 balanceBefore = balanceOf(token);
+        console.log("balanceBefore: ", balanceBefore);
         IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+        uint256 balanceAfter = balanceOf(token);
+        console.log("balanceBefore: ", balanceAfter);
+        amount = balanceAfter - balanceBefore;
         sendDepositMessage(token, amount, to, data, msg.value);
     }
 
