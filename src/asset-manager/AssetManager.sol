dff30910bdfcb1d65e38381628003886f6186ef9
gcranju <qa5505815@gmail.com>
2024-06-17 10:25:40 +0545

    chore: removed console log

--- src/asset-manager/AssetManager.sol
+++ src/asset-manager/AssetManager.sol
@@ -139,7 +139,6 @@ contract AssetManager is
         uint amount,
         string memory to
     ) external payable {
-        console.log("to: ", to);
         _deposit(token, amount, to, "");
     }
 
@@ -176,10 +175,8 @@ contract AssetManager is
     ) internal {
         require(amount > 0, "Amount less than minimum amount");
         uint256 balanceBefore = balanceOf(token);
-        console.log("balanceBefore: ", balanceBefore);
         IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
         uint256 balanceAfter = balanceOf(token);
-        console.log("balanceBefore: ", balanceAfter);
         amount = balanceAfter - balanceBefore;
         sendDepositMessage(token, amount, to, data, msg.value);
     }
